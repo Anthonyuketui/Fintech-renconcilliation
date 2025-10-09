99c45772f5893c0cdfc5ce36afad215ad445f308
AnthonyUketui <uketuianthony@gmail.com>
2025-10-09 18:07:15 +0100

    added deployment config

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -1,5 +1,11 @@
+resource "random_string" "secret_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 resource "aws_secretsmanager_secret" "db_password" {
-  name        = "${var.project_name}-${var.environment}-db-password"
+  name        = "${var.project_name}-${var.environment}-db-password-${random_string.secret_suffix.result}"
   description = "Database password for ${var.project_name} ${var.environment}"
 
   tags = merge(var.tags, {
