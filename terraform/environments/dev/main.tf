7880d53a937ed6bafb0e3d67c1b321cf16c6217c
AnthonyUketui <uketuianthony@gmail.com>
2025-10-17 16:10:41 +0100

    Infusing security

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -87,10 +87,19 @@ resource "aws_security_group" "ecs" {
 
   # Allow all outbound traffic for AWS services
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS outbound for AWS services"
+  }
+  
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP outbound for package updates"
     description = "All outbound traffic"
   }
 
