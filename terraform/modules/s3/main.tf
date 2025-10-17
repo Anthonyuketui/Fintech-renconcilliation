4838cb556cbacd893ecde69e3b46cf5c656c7a80
AnthonyUketui <uketuianthony@gmail.com>
2025-10-17 16:10:41 +0100

    Infusing security

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -7,6 +7,15 @@ resource "aws_s3_bucket" "reports" {
   })
 }
 
+resource "aws_s3_bucket_public_access_block" "reports" {
+  bucket = aws_s3_bucket.reports.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_versioning" "reports" {
   bucket = aws_s3_bucket.reports.id
   versioning_configuration {
